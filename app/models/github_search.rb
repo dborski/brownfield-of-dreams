0e7eab4dd2ad8a1355d62b5c114f0557bc1520cd
Derek Borski <derek.borski@gmail.com>
2020-06-30 17:54:43 -0600

    Debug travis ci

--- app/models/github_search.rb
+++ app/models/github_search.rb
@@ -2,9 +2,9 @@ class GithubSearch
 
   def repos(token)
     repos = service.get_repos(token).map do |data|
+      puts data
       GithubRepo.new(data)
     end
-    puts repos
     repos
   end 
 
