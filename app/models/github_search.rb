5f7852c852d6ab582650738a243bc30f40ef46f3
Ethan Hocking <eshocking253@gmail.com>
2020-07-01 10:54:38 -0600

    Add folowers search method to github search class

--- app/models/github_search.rb
+++ app/models/github_search.rb
@@ -4,9 +4,15 @@ class GithubSearch
     service.get_repos(token).map do |data|
       GithubRepo.new(data)
     end
-  end 
+  end
+
+  def followers(token)
+    service.get_followers(token).map do |data|
+      GithubUser.new(data)
+    end
+  end
 
   def service
     GithubService.new
-  end 
-end 
+  end
+end
