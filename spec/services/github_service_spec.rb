20798c0c077e9cf649b87b07db30a70acb272bdf
Ethan Hocking <eshocking253@gmail.com>
2020-07-01 10:32:02 -0600

    Add spec for github followers api call

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -10,4 +10,13 @@ describe 'Github API' do
     expect(repos.first[:name]).to eq("brownfield-of-dreams")
     expect(repos.count).to eq(30)
   end
+
+  it "sends followers for a single user" do
+    token = ENV['GITHUB_API_KEY']
+    github = GithubService.new
+    followers = github.get_followers(token)
+
+    expect(followers.first[:login]).to eq("alex-latham")
+    expect(followers.count).to eq(3)
+  end
 end
