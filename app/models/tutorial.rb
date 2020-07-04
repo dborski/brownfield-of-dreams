ef4e9c3201faa734c2cbc9f9b04c49b30b4ea872
Derek Borski <derek.borski@gmail.com>
2020-07-04 16:25:22 -0600

    create_playlist and create_playlist_videos methods

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,27 @@ class Tutorial < ApplicationRecord
   has_many :videos, -> { order(position: :ASC) }, inverse_of: :tutorial
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  def create_playlist(playlist_id)
+    playlist_json = YoutubeService.new.playlist_info(playlist_id)
+    playlist_info = playlist_json[:items][0]
+    new_tutorial = Tutorial.create(
+      title:       playlist_info[:snippet][:title],
+      description: playlist_info[:snippet][:description],
+      thumbnail:   playlist_info[:snippet][:thumbnails][:high][:url],
+      playlist_id: playlist_info[:id])
+  end 
+
+  def create_playlist_videos(playlist_id, new_tutorial)
+    playlist_videos_json = YoutubeService.new.playlist_videos_info(playlist_id)
+    playlist_videos_json[:items].each.with_index(1) do |vid, index|
+      new_tutorial.videos.create!(
+        title:       vid[:snippet][:title],
+        description: vid[:snippet][:description],
+        thumbnail:   vid[:snippet][:thumbnails][:high][:url],
+        video_id:    vid[:snippet][:resourceId][:videoId],
+        position:    index
+      )
+    end
+  end 
 end
