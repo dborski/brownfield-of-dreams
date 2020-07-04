4296240fa1d81bab039f729e99a4264b771dc260
Derek Borski <derek.borski@gmail.com>
2020-07-04 16:26:16 -0600

    Start spec

--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
 
 describe 'Youtube API' do
-  it 'sends repos for a single user' do
+  it 'gets playlist info' do
     id = "PL4pGkk3QBW28n6QCBWcmE76Ro7ONEzlMG"
     youtube = YoutubeService.new
-    playlists = youtube.playlist_info(id)
-    playlist_items = youtube.playlist_videos_info(id)
+    playlist_json = youtube.playlist_info(id)
+    playlist_info = playlist_json[:items][0]
 
-    binding.pry
-    
-    expect(playlist_items).to be_a(Array)
-    expect(playlist_items.first).to have_key(:name)
-    expect(playlist_items.first).to have_key(:html_url)
+    expect(playlist_info[:snippet]).to have_key(:title)
+    expect(playlist_info[:snippet]).to have_key(:description)
+    expect(playlist_info[:snippet][:thumbnails][:high]).to have_key(:url)
+    expect(playlist_info).to have_key(:id)
   end
 
-  # xit "sends followers for a single user" do
-  #   id = ENV['GITHUB_API_KEY']
-  #   github = GithubService.new
-  #   followers = github.get_followers(token)
+  xit 'gets playlist videos info' do
+    id = "PL4pGkk3QBW28n6QCBWcmE76Ro7ONEzlMG"
+    youtube = YoutubeService.new
+    playlist_videos_json = youtube.playlist_videos_info(id)
+    playlist_videos_info = playlist_videos_json[:items]
 
-  #   expect(followers.first[:login]).to eq("alex-latham")
-  #   expect(followers.count).to eq(3)
-  # end
+    binding.pry
+
+    expect(playlist_videos_info).to be_a(Array)
+    expect(playlist_videos_info.first[:vid]).to have_key(:title)
+    expect(playlist_videos_info.first).to have_key(:description)
+    expect(playlist_videos_info.first).to have_key(:thumbnail)
+    expect(playlist_videos_info.first).to have_key(:playlist_id)
+    expect(playlist_videos_info.first).to have_key(:classroom)
+  end
 end
