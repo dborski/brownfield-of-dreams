99bbc8ae21c0c180c3bf7a907ab5d490efcbdeaa
Derek Borski <derek.borski@gmail.com>
2020-07-04 16:38:31 -0600

    Finish spec

--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -13,19 +13,16 @@ describe 'Youtube API' do
     expect(playlist_info).to have_key(:id)
   end
 
-  xit 'gets playlist videos info' do
+  it 'gets playlist videos info' do
     id = "PL4pGkk3QBW28n6QCBWcmE76Ro7ONEzlMG"
     youtube = YoutubeService.new
     playlist_videos_json = youtube.playlist_videos_info(id)
     playlist_videos_info = playlist_videos_json[:items]
 
-    binding.pry
-
     expect(playlist_videos_info).to be_a(Array)
-    expect(playlist_videos_info.first[:vid]).to have_key(:title)
-    expect(playlist_videos_info.first).to have_key(:description)
-    expect(playlist_videos_info.first).to have_key(:thumbnail)
-    expect(playlist_videos_info.first).to have_key(:playlist_id)
-    expect(playlist_videos_info.first).to have_key(:classroom)
+    expect(playlist_videos_info.first[:snippet]).to have_key(:title)
+    expect(playlist_videos_info.first[:snippet]).to have_key(:description)
+    expect(playlist_videos_info.first[:snippet][:thumbnails][:high]).to have_key(:url)
+    expect(playlist_videos_info.first[:snippet][:resourceId]).to have_key(:videoId)
   end
 end
