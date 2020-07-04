94e3ff116280a3014dd35c38c00d3f407f7c82e3
Ethan Hocking <eshocking253@gmail.com>
2020-07-04 10:25:35 -0600

    Add spec for github oauth section on user dashboard

---
+++ spec/features/user/user_can_connect_to_github_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "User" do
+  it "User can see followers while logged in" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+        'provider' => 'github',
+        'credentials' => {'token' => ENV['GITHUB_API_KEY']}
+      })
+
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    user.github_token = nil
+    user.save
+
+    visit "/dashboard"
+
+    click_on "Connect to Github"
+
+    within ".github" do
+      expect(page).to have_css(".followers")
+    end
+
+    OmniAuth.config.mock_auth[:github] = nil
+  end
+end
