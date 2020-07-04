b3e5c62e3ba711df6ef3030f5e06e45b22efbea7
Derek Borski <derek.borski@gmail.com>
2020-07-04 16:46:24 -0600

    Add specs for create_playlists and create_playlist_videos

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'instance_methods' do
+    it 'create_playlists' do
+      id = "PL4pGkk3QBW28n6QCBWcmE76Ro7ONEzlMG"
+      tutorial = create(:tutorial)
+
+      expect(tutorial.create_playlist(id)).to be_a(Tutorial) 
+    end
+    it 'create_playlist_videos' do
+      id = "PL4pGkk3QBW28n6QCBWcmE76Ro7ONEzlMG"
+      tutorial = create(:tutorial)
+
+      expect(tutorial.create_playlist_videos(id).count).to be_eq(50) 
+      expect(tutorial.create_playlist_videos(id).first).to be_a(Video) 
+    end
+  end
 end
