c0a5488b96e7d9284ea09d9ffe1650e4bef9dab8
Derek Borski <derek.borski@gmail.com>
2020-07-05 02:30:51 -0600

    Remove commented out user story

--- spec/features/admin/new_tutorial_spec.rb
+++ spec/features/admin/new_tutorial_spec.rb
@@ -39,17 +39,3 @@ RSpec.describe 'New tutorial', type: :feature do
     end
   end 
 end 
-# As an admin
-# When I visit '/admin/tutorials/new'
-# Then I should see a link for 'Import YouTube Playlist'
-# When I click 'Import YouTube Playlist'
-# Then I should see a form
-
-# And when I fill in 'Playlist ID' with a valid ID
-# Then I should be on '/admin/dashboard'
-# And I should see a flash message that says 'Successfully created tutorial. View it here.'
-# And 'View it here' should be a link to '/tutorials/:id'
-# And when I click on 'View it here'
-# Then I should be on '/tutorials/:id'
-# And I should see all videos from the YouTube playlist
-# And the order should be the same as it was on YouTube
