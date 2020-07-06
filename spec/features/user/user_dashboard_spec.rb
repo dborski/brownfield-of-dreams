6cc094f1444263efacffafc66c34105c77e3d794
Derek Borski <derek.borski@gmail.com>
2020-07-06 16:46:11 -0600

    Remove user story and clean up

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -49,9 +49,3 @@ RSpec.describe 'user dashboard show page', type: :feature do
     end
   end 
 end 
-
-# As a logged in user
-# When I visit '/dashboard'
-# Then I should see a list of all bookmarked segments under the Bookmarked Segments section
-# And they should be organized by which tutorial they are a part of
-# And the videos should be ordered by their position
