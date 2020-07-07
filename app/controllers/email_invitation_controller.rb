16474ce663bdb1daa94d1700b9fd06463fd665ca
Derek Borski <derek.borski@gmail.com>
2020-07-07 14:13:04 -0600

    Update from e-mail address

--- app/controllers/email_invitation_controller.rb
+++ app/controllers/email_invitation_controller.rb
@@ -2,6 +2,17 @@ class EmailInvitationController < ApplicationController
   def new;end
 
   def create
+    github_handle = params[:github_handle]
+
+    email_info = { user_token: current_user.github_token,
+                   app: "Brownfield of Dreams",
+                 }
+
+    if InvitationMailer.invite(email_info, github_handle).deliver_now == nil
+      flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
+    else 
+      flash[:notice] = "Successfully sent invite!"
+    end
 
     redirect_to dashboard_path
   end 
