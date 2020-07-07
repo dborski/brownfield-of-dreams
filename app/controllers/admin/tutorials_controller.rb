2f276fe4e82d73e141d607344125910a2692be00
Ethan Hocking <eshocking253@gmail.com>
2020-07-07 10:42:34 -0600

    Update tutorial create action to account for tutorial creation without playlist id

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -4,14 +4,22 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   def create
-    playlist_id = params[:tutorial][:playlist_id]
+    begin
+      playlist_id = params[:tutorial][:playlist_id]
 
-    if playlist_id
-      tutorial = Tutorial.new.create_playlist(playlist_id)
-      Tutorial.new.create_playlist_videos(playlist_id, tutorial)
+      if playlist_id
+        tutorial = Tutorial.new.create_playlist(playlist_id)
+        Tutorial.new.create_playlist_videos(playlist_id, tutorial)
+        flash[:success] = "Successfully created tutorial. #{view_context.link_to('View it here', tutorial_path(tutorial))}"
+      else
+        tutorial = Tutorial.create(tutorial_params)
+        flash[:success] = "Successfully created tutorial."
+      end
+      redirect_to admin_dashboard_path
+    rescue StandardError
+      flash[:error] = "There was an issue creating the tutorial."
+      redirect_to tutorial_new_path
     end
-    flash[:success] = "Successfully created tutorial. #{view_context.link_to('View it here', tutorial_path(tutorial))}"
-    redirect_to admin_dashboard_path
   end
 
   def new
@@ -28,7 +36,7 @@ class Admin::TutorialsController < Admin::BaseController
 
   def destroy
     tutorial = Tutorial.find(params[:id])
-    flash[:success] = "#{tutorial.title} tagged!" if tutorial.destroy
+    flash[:success] = "#{tutorial.title} deleted." if tutorial.destroy
     redirect_to admin_dashboard_path
   end
 
