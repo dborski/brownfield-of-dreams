f1b9e990670049f956f14b8e41dcb8491f7e88bd
Ethan Hocking <eshocking253@gmail.com>
2020-07-08 09:37:07 -0600

    Add spec for account confirmation mailer

---
+++ spec/mailers/account_confirmation_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe AccountConfirmationMailer, type: :mailer do
+  def get_message_part(mail, content_type)
+    mail.body.parts.find { |p| p.content_type.match content_type }.body.raw_source
+  end
+
+  describe "account confirmation email" do
+    it "Sends an email with a link to user for activation" do
+      user = create(:user, github_token: ENV["GITHUB_API_KEY"])
+
+      AccountConfirmationMailer.confirm_account(user).deliver_now
+
+      activation_email = ActionMailer::Base.deliveries.last
+      email = Capybara::Node::Simple.new(get_message_part(activation_email, /html/))
+
+      url = url_for( controller:'activation', action:'activate', host: ENV['HOST'])
+      expect(email).to have_link('Visit here to activate your account.', href: url)
+    end
+  end
+end
