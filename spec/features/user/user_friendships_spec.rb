225c31e5e8d7d299d299751c5ac1be3c40857769
Ethan Hocking <eshocking253@gmail.com>
2020-07-08 16:31:42 -0600

    add user friendships spec

--- spec/features/user/user_friendships_spec.rb
+++ spec/features/user/user_friendships_spec.rb
@@ -1,14 +1,42 @@
 require "rails_helper"
 
 describe "As a registered user with a github token", :vcr do
-  it "I can add friends" do
-    user = create(:user, github_token: ENV['GITHUB_TOKEN1'])
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  before :each do
+    user1 = create(:user, github_token: ENV['GITHUB_TOKEN1'], github_username: 'Shock253')
+    user2 = create(:user, github_token: ENV['GITHUB_TOKEN2'], github_username: 'dborski')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
 
     visit '/dashboard'
+  end
 
-    within ".follower-#{}" do
-      # expect button
+  it "buttons to friend users show up on dashboard" do
+    within ".followed-dborski" do
+      expect(page).to have_button('Add as Friend')
+    end
+
+    within ".follower-dborski" do
+      expect(page).to have_button('Add as Friend')
+    end
+
+    within ".follower-alex-latham" do
+      expect(page).to_not have_button('Add as Friend')
+    end
+  end
+
+  xit "I can add friends" do
+    within ".follower-dborski" do
+      expect(page).to have_button('Add as Friend')
+      click_button 'Add as Friend'
+    end
+
+    expect(current_path).to eq('/dashboard')
+
+    within ".follower-dborski" do
+      expect(page).to_not have_button('Add as Friend')
+    end
+
+    within '.friends' do
+      expect(page).to have_content(user2.name)
     end
   end
 end
